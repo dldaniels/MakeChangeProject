73644e47002947456d28aab35ce32ee56519b73c
David Daniels <dldaniels07@yahoo.com>
2020-08-30 00:49:21 -0600

    MakeChange

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -8,7 +8,7 @@ public class MakeChange {
 		// Variables to store customer input
 		double price = 0.00;
 		double amountGiven = 0.00;
-		// Initialize constant variables to define currency values
+		// Initialize variables to define currency values
 		double ten = 10.00;
 		double five = 5.00;
 		double one = 1.00;
@@ -20,9 +20,9 @@ public class MakeChange {
 		// quit
 		boolean keepGoing = true;
 		// Greet the customer
-		System.out.println("Hello, I am Apu! Welcome to the Kwick E Mart!" + "\n" + 
-		"Convienience forever, freshness never!"); 
-			
+		System.out.println(
+				"Hello, I am Apu! Welcome to the Kwick E Mart!" + "\n" + "Convienience forever, freshness never!");
+
 		// Prompt user for purchase price and amount tendered
 		while (keepGoing) {
 			System.out.print("Please enter the price of the item: ");
@@ -52,11 +52,16 @@ public class MakeChange {
 			// In this section we will calculate the amount of each individual bill or
 			// coin returned to customer. Double variable is cast to an Integer and
 			// displayed on screen to report how many of each bill or coin will be returned
+
+			//If changeReturned divided by each currency amount is greater than zero the whole number amount of that currency is extracted and 
+			//stored as an integer. changeReturned is then reassigned to change returned of that modulo amount.
 			if ((changeReturned / ten) > 0) {
 				int tensReturned = (int) (changeReturned / ten);
+				//during testing it was discovered that some calculations were off due to double rounding flaws, adding 0.0001 was able to correct the errors
 				changeReturned = (changeReturned % ten) + 0.0001;
+				//Nested if used to determine if it is necessary to print value. For better output formatting
 				if (tensReturned == 0) {
-
+				
 				} else if (tensReturned == 1) {
 					System.out.println("Please give the customer " + tensReturned + " Ten dollar bill.");
 				} else if (tensReturned > 1) {
@@ -142,6 +147,7 @@ public class MakeChange {
 					System.out.println("Please give the customer " + pennyReturned + " Pennies.");
 				}
 			}
+
 			// User is prompted to enter if they are finished or wish to continue
 			System.out.print(
 					"Do you have any other purchases? " + "May I suggest a discounted 3 day old hot dog? Enter Y/N: ");
@@ -175,40 +181,3 @@ public class MakeChange {
 		kb.close();
 	}
 }
-
-//Make Change (Cash Register)
-
-//Overview
-//In the cash register we will calculate the amount of change returned to a customer based on the purchase price and the amount tendered. We will also notify the attendant how many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer. Change should be provided using the largest bill and coin denominations as possible. Denominations that are not used should not be displayed.
-//
-//Hint: Mod operator
-//
-//User Story #1
-//The user is prompted asking for the price of the item.
-//
-//User Story #2
-//The user is then prompted asking how much money was tendered by the customer.
-//
-//User Story #3
-//Display an appropriate message if the customer provided too little money or the exact amount.
-//
-//User Story #4
-//If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
-//
-//Grading
-//This is a graded project. You are expected to have your project completed by the start of class on Monday morning.
-//
-//You will be given either a pass or fail based on whether your code works given all of the following test conditions:
-//
-//Amount: .67, Tendered: .50, Result: Error message
-//Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
-//Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
-//Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
-//Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations for correct change.
-//If the project does work with all of the above test conditions, you will be given a 1 for this week's project.
-//
-//If the project does not work with the above test conditions, you will be given a 0 for this week's project.
-//
-//If you get a zero on the project, you can upgrade to a score of .5 if you turn in a working project by the start of class on the following Monday morning AND notify an instructor that you wish to get partial credit.
-//
-//To turn in a project, you must push it to GitHub. You must include a README.md that describes how to run your program.
